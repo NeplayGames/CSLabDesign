0e95f3e60af23a4afc19723e411c5162ac712e24
Najshree <81029735+NeplayGames@users.noreply.github.com>
2024-05-16 10:25:30 +0545

    This commit consist of a simple application that allows us to add computers to our room for design purpose.

---
+++ Design/DesignApplication/Assets/Scripts/ItemSpawner.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject template;
+    [SerializeField] private LayerMask layer;
+
+    private GameObject prefab;
+    
+    private Transform mainCameraTransform {get {return Camera.main.transform;}}
+
+    //Temporary should be handle by UI Component.
+    void Awake() => Build();
+    
+    void Update()
+    {
+        MovePreview();
+        if (Input.GetMouseButtonDown(0))
+            Build();
+        if (Input.GetKey(KeyCode.Escape))
+            Application.Quit();
+        if(Input.GetMouseButtonDown(1))
+            Rotate();          
+    }
+
+    private void MovePreview()
+    {
+        if (Physics.Raycast(mainCameraTransform.position, mainCameraTransform.forward, out RaycastHit hit, 30f, layer))
+        {
+            prefab.transform.position = new Vector3(
+                        Mathf.Round(hit.point.x),
+                        Mathf.Round(hit.point.y) + hit.transform.localScale.y * .5f,
+                        Mathf.Round(hit.point.z)
+                    );
+        }
+    }
+
+    private void Build() => prefab = Instantiate(template);
+    
+    public void Rotate() => prefab.transform.Rotate(new Vector3(0, 90, 0), Space.Self);    
+}
---
+++ Design/DesignApplication/Assets/Scripts/Movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float MovementSpeed = 10f;
+    [SerializeField] private float lookSpeed = 3;
+    private Vector2 rotation = Vector2.zero;
+
+    private Transform cameraTransform;
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        cameraTransform = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x += -Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector2(0, rotation.y) * lookSpeed;
+        Quaternion cameraRotation = Quaternion.Euler(rotation.x * lookSpeed, 0, 0);
+
+        cameraTransform.localRotation = cameraRotation;
+    }
+
+    void FixedUpdate()
+    {
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float virticalMove = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(horizontalMove, 0, virticalMove);
+        move = transform.TransformDirection(move);
+        move *= MovementSpeed;
+        move *= Time.deltaTime;
+
+        transform.position += move;
+    }
+}
